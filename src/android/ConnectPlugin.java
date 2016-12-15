5c6cc350976c50b1f743f2b89a5dc0b1437d3abe
Andres Arslanian <andres.arslanian@gmail.com>
2016-12-15 13:20:34 -0300

    bug fix

--- src/android/ConnectPlugin.java
+++ src/android/ConnectPlugin.java
@@ -742,7 +742,7 @@ public class ConnectPlugin extends CordovaPlugin {
             return;
         }
         Bundle payload = jsonToBundle(args.getJSONObject(0));
-        Log.d(TAG, args.getJSONObject(0));
+        Log.d(TAG, args.getJSONObject(0).toString());
         logger.logPushNotificationOpen(payload);
 
         if (NotificationsManager.canPresentCard(payload)) {

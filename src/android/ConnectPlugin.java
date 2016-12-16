dd7903d960a2afe6dd9f3e72b6d84a102d3ef5f1
Andres Arslanian <andres.arslanian@gmail.com>
2016-12-16 13:22:22 -0300

    bug fix

--- src/android/ConnectPlugin.java
+++ src/android/ConnectPlugin.java
@@ -257,7 +257,7 @@ public class ConnectPlugin extends CordovaPlugin {
     }
 
     @Override
-    protected void onNewIntent(Intent intent) {
+    public void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
 
         setIntent(intent);
